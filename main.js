040eb8a11c2e02b1db33762cd0c6a73f0edf894e
halloweenjack <kat@hjcg.com>
2022-08-24 11:43:43 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,465 @@
+const table =
+  [
+    {
+      'date': '2022/07/17',
+      'photo': '2022/07/17/12/DomeFuji01_20220717120758.jpg',
+      'mov': 'DomeFuji01_20220717120758.mp4',
+      'st': '2022-07-16T15:00:00Z',
+      'et': '2022-07-17T15:00:00Z',
+      'ct': '2022-07-17T03:00:00Z',
+      'col': 1,
+      'row': 1,
+    },
+    {
+      'date': '2022/07/18',
+      'photo': '2022/07/18/12/DomeFuji01_20220718120018.jpg',
+      'mov': 'DomeFuji01_20220718120018.mp4',
+      'st': '2022-07-17T15:00:00Z',
+      'et': '2022-07-18T15:00:00Z',
+      'ct': '2022-07-18T03:00:00Z',
+      'col': 2,
+      'row': 1,
+    },
+    {
+      'date': '2022/07/19',
+      'photo': '2022/07/19/12/DomeFuji01_20220719120001.jpg',
+      'mov': 'DomeFuji01_20220719120001.mp4',
+      'st': '2022-07-18T15:00:00Z',
+      'et': '2022-07-19T15:00:00Z',
+      'ct': '2022-07-19T03:00:00Z',
+      'col': 3,
+      'row': 1,
+    },
+    {
+      'date': '2022/07/20',
+      'photo': '2022/07/20/12/DomeFuji01_20220720120003.jpg',
+      'mov': 'DomeFuji01_20220720120003.mp4',
+      'st': '2022-07-19T15:00:00Z',
+      'et': '2022-07-20T15:00:00Z',
+      'ct': '2022-07-20T03:00:00Z',
+      'col': 4,
+      'row': 1,
+    },
+    {
+      'date': '2022/07/21',
+      'photo': '2022/07/21/12/DomeFuji01_20220721120002.jpg',
+      'mov': 'DomeFuji01_20220721120002.mp4',
+      'st': '2022-07-20T15:00:00Z',
+      'et': '2022-07-21T15:00:00Z',
+      'ct': '2022-07-21T03:00:00Z',
+      'col': 5,
+      'row': 1,
+    },
+    {
+      'date': '2022/07/22',
+      'photo': '2022/07/22/12/DomeFuji01_20220722120002.jpg',
+      'mov': 'DomeFuji01_20220722120002.mp4',
+      'st': '2022-07-21T15:00:00Z',
+      'et': '2022-07-22T15:00:00Z',
+      'ct': '2022-07-22T03:00:00Z',
+      'col': 6,
+      'row': 1,
+    },
+    {
+      'date': '2022/07/23',
+      'photo': '2022/07/23/12/DomeFuji01_20220723120001.jpg',
+      'mov': 'DomeFuji01_20220723120001.mp4',
+      'st': '2022-07-22T15:00:00Z',
+      'et': '2022-07-23T15:00:00Z',
+      'ct': '2022-07-23T03:00:00Z',
+      'col': 7,
+      'row': 1,
+    },
+    {
+      'date': '2022/07/24',
+      'photo': '2022/07/24/12/DomeFuji01_20220724120003.jpg',
+      'mov': 'DomeFuji01_20220724120003.mp4',
+      'st': '2022-07-23T15:00:00Z',
+      'et': '2022-07-24T15:00:00Z',
+      'ct': '2022-07-24T03:00:00Z',
+      'col': 1,
+      'row': 2,
+    },
+    {
+      'date': '2022/07/25',
+      'photo': '2022/07/25/09/DomeFuji01_20220725090010.jpg',
+      'mov': 'DomeFuji01_20220725090010.mp4',
+      'st': '2022-07-24T15:00:00Z',
+      'et': '2022-07-25T15:00:00Z',
+      'ct': '2022-07-25T03:00:00Z',
+      'col': 2,
+      'row': 2,
+    },
+    {
+      'date': '2022/07/26',
+      'photo': '2022/07/26/12/DomeFuji01_20220726120004.jpg',
+      'mov': 'DomeFuji01_20220726120004.mp4',
+      'st': '2022-07-25T15:00:00Z',
+      'et': '2022-07-26T15:00:00Z',
+      'ct': '2022-07-26T03:00:00Z',
+      'col': 3,
+      'row': 2,
+    },
+    {
+      'date': '2022/07/27',
+      'photo': '2022/07/27/12/DomeFuji01_20220727120006.jpg',
+      'mov': 'DomeFuji01_20220727120006.mp4',
+      'st': '2022-07-26T15:00:00Z',
+      'et': '2022-07-27T15:00:00Z',
+      'ct': '2022-07-27T03:00:00Z',
+      'col': 4,
+      'row': 2,
+    },
+    {
+      'date': '2022/07/28',
+      'photo': '2022/07/28/12/DomeFuji01_20220728120027.jpg',
+      'mov': 'DomeFuji01_20220728120027.mp4',
+      'st': '2022-07-27T15:00:00Z',
+      'et': '2022-07-28T15:00:00Z',
+      'ct': '2022-07-28T03:00:00Z',
+      'col': 5,
+      'row': 2,
+    },
+    {
+      'date': '2022/07/29',
+      'photo': '2022/07/29/12/DomeFuji01_20220729120018.jpg',
+      'mov': 'DomeFuji01_20220729120018.mp4',
+      'st': '2022-07-28T15:00:00Z',
+      'et': '2022-07-29T15:00:00Z',
+      'ct': '2022-07-29T03:00:00Z',
+      'col': 6,
+      'row': 2,
+    },
+    {
+      'date': '2022/07/30',
+      'photo': '2022/07/30/12/DomeFuji01_20220730120013.jpg',
+      'mov': 'DomeFuji01_20220730120013.mp4',
+      'st': '2022-07-29T15:00:00Z',
+      'et': '2022-07-30T15:00:00Z',
+      'ct': '2022-07-30T03:00:00Z',
+      'col': 7,
+      'row': 2,
+    },
+    {
+      'date': '2022/07/31',
+      'photo': '2022/07/31/12/DomeFuji01_20220731120003.jpg',
+      'mov': 'DomeFuji01_20220731120003.mp4',
+      'st': '2022-07-30T15:00:00Z',
+      'et': '2022-07-31T15:00:00Z',
+      'ct': '2022-07-31T03:00:00Z',
+      'col': 1,
+      'row': 3,
+    },
+    {
+      'date': '2022/08/01',
+      'photo': '2022/08/01/12/DomeFuji01_20220801120000.jpg',
+      'mov': 'DomeFuji01_20220801120000.mp4',
+      'st': '2022-07-31T15:00:00Z',
+      'et': '2022-08-01T15:00:00Z',
+      'ct': '2022-08-01T03:00:00Z',
+      'col': 2,
+      'row': 3,
+    },
+    {
+      'date': '2022/08/02',
+      'photo': '2022/08/02/12/DomeFuji01_20220802120007.jpg',
+      'mov': 'DomeFuji01_20220802120007.mp4',
+      'st': '2022-08-01T15:00:00Z',
+      'et': '2022-08-02T15:00:00Z',
+      'ct': '2022-08-02T03:00:00Z',
+      'col': 3,
+      'row': 3,
+    },
+    {
+      'date': '2022/08/03',
+      'photo': '2022/08/03/12/DomeFuji01_20220803120003.jpg',
+      'mov': 'DomeFuji01_20220803120003.mp4',
+      'st': '2022-08-02T15:00:00Z',
+      'et': '2022-08-03T15:00:00Z',
+      'ct': '2022-08-03T03:00:00Z',
+      'col': 4,
+      'row': 3,
+    },
+    {
+      'date': '2022/08/04',
+      'photo': '2022/08/04/12/DomeFuji01_20220804120003.jpg',
+      'mov': 'DomeFuji01_20220804120003.mp4',
+      'st': '2022-08-03T15:00:00Z',
+      'et': '2022-08-04T15:00:00Z',
+      'ct': '2022-08-04T03:00:00Z',
+      'col': 5,
+      'row': 3,
+    },
+    {
+      'date': '2022/08/05',
+      'photo': '2022/08/05/12/DomeFuji01_20220805120010.jpg',
+      'mov': 'DomeFuji01_20220805120010.mp4',
+      'st': '2022-08-04T15:00:00Z',
+      'et': '2022-08-05T15:00:00Z',
+      'ct': '2022-08-05T03:00:00Z',
+      'col': 6,
+      'row': 3,
+    },
+    {
+      'date': '2022/08/06',
+      'photo': '2022/08/06/12/DomeFuji01_20220806120008.jpg',
+      'mov': 'DomeFuji01_20220806120008.mp4',
+      'st': '2022-08-05T15:00:00Z',
+      'et': '2022-08-06T15:00:00Z',
+      'ct': '2022-08-06T03:00:00Z',
+      'col': 7,
+      'row': 3,
+    },
+    {
+      'date': '2022/08/07',
+      'photo': '2022/08/07/12/DomeFuji01_20220807120007.jpg',
+      'mov': 'DomeFuji01_20220807120007.mp4',
+      'st': '2022-08-06T15:00:00Z',
+      'et': '2022-08-07T15:00:00Z',
+      'ct': '2022-08-07T03:00:00Z',
+      'col': 1,
+      'row': 4,
+    },
+    {
+      'date': '2022/08/08',
+      'photo': '2022/08/08/12/DomeFuji01_20220808120004.jpg',
+      'mov': 'DomeFuji01_20220808120004.mp4',
+      'st': '2022-08-07T15:00:00Z',
+      'et': '2022-08-08T15:00:00Z',
+      'ct': '2022-08-08T03:00:00Z',
+      'col': 2,
+      'row': 4,
+    },
+    {
+      'date': '2022/08/09',
+      'photo': '2022/08/09/12/DomeFuji01_20220809120003.jpg',
+      'mov': 'DomeFuji01_20220809120003.mp4',
+      'st': '2022-08-08T15:00:00Z',
+      'et': '2022-08-09T15:00:00Z',
+      'ct': '2022-08-09T03:00:00Z',
+      'col': 3,
+      'row': 4,
+    },
+    {
+      'date': '2022/08/10',
+      'photo': '2022/08/10/12/DomeFuji01_20220810120000.jpg',
+      'mov': 'DomeFuji01_20220810120000.mp4',
+      'st': '2022-08-09T15:00:00Z',
+      'et': '2022-08-10T15:00:00Z',
+      'ct': '2022-08-10T03:00:00Z',
+      'col': 4,
+      'row': 4,
+    },
+    {
+      'date': '2022/08/11',
+      'photo': '2022/08/11/12/DomeFuji01_20220811120003.jpg',
+      'mov': 'DomeFuji01_20220811120003.mp4',
+      'st': '2022-08-10T15:00:00Z',
+      'et': '2022-08-11T15:00:00Z',
+      'ct': '2022-08-11T03:00:00Z',
+      'col': 5,
+      'row': 4,
+    },
+    {
+      'date': '2022/08/12',
+      'photo': '2022/08/12/12/DomeFuji01_20220812120048.jpg',
+      'mov': 'DomeFuji01_20220812120048.mp4',
+      'st': '2022-08-11T15:00:00Z',
+      'et': '2022-08-12T15:00:00Z',
+      'ct': '2022-08-12T03:00:00Z',
+      'col': 6,
+      'row': 4,
+    },
+    {
+      'date': '2022/08/13',
+      'photo': '2022/08/13/12/DomeFuji01_20220813120017.jpg',
+      'mov': 'DomeFuji01_20220813120017.mp4',
+      'st': '2022-08-12T15:00:00Z',
+      'et': '2022-08-13T15:00:00Z',
+      'ct': '2022-08-13T03:00:00Z',
+      'col': 7,
+      'row': 4,
+    },
+    {
+      'date': '2022/08/14',
+      'photo': '2022/08/14/12/DomeFuji01_20220814120004.jpg',
+      'mov': 'DomeFuji01_20220814120004.mp4',
+      'st': '2022-08-13T15:00:00Z',
+      'et': '2022-08-14T15:00:00Z',
+      'ct': '2022-08-14T03:00:00Z',
+      'col': 1,
+      'row': 5,
+    },
+    {
+      'date': '2022/08/15',
+      'photo': '2022/08/15/12/DomeFuji01_20220815120147.jpg',
+      'mov': 'DomeFuji01_20220731120003.mp4',
+      'st': '2022-08-14T15:00:00Z',
+      'et': '2022-08-15T15:00:00Z',
+      'ct': '2022-08-15T03:00:00Z',
+      'col': 2,
+      'row': 5,
+    },
+    {
+      'date': '2022/08/16',
+      'photo': '2022/08/16/12/DomeFuji01_20220816120121.jpg',
+      'mov': 'DomeFuji01_20220816120121.mp4',
+      'st': '2022-08-15T15:00:00Z',
+      'et': '2022-08-16T15:00:00Z',
+      'ct': '2022-08-16T03:00:00Z',
+      'col': 3,
+      'row': 5,
+    },
+  ];
+
+import * as THREE from 'three';
+
+import { TWEEN } from 'https://unpkg.com/three@0.139.2/examples/jsm/libs/tween.module.min.js';
+import { MapControls } from 'https://unpkg.com/three@0.139.2/examples/jsm/controls/OrbitControls.js';
+import { CSS3DRenderer, CSS3DObject } from 'https://unpkg.com/three@0.139.2/examples/jsm/renderers/CSS3DRenderer.js';
+
+const photoPath = "https://jh170034.kudpc.kyoto-u.ac.jp/hpvt/DomeFuji/data/picture_m/DomeFuji01/";
+const movPath = "https://jh170034.kudpc.kyoto-u.ac.jp/hpvt/DomeFuji/data/oneDayMovie/DomeFuji01/";
+const viewUrl = "http://k2go.jp/public/VideoViewer/1.0.1/";
+const parm = "&ft=60000&json=false&m1=Mt.Fuji&bg=rgb(0,0,0)";
+
+let camera, scene, renderer;
+let controls;
+
+const objects = [];
+const targets = { table: [] };
+
+init();
+animate();
+
+function init() {
+
+  camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 10000);
+  camera.position.z = 1500;
+  camera.position.y = -500;
+  camera.position.x = 1500;
+  // camera.target = new THREE.Vector3(100, 0, 0);
+  // camera.lookAt(camera.target);
+
+  scene = new THREE.Scene();
+
+  for (let data in table) {
+    const box = document.createElement('div');
+    box.className = 'box';
+    box.style.backgroundColor = 'rgba(0,127,127,' + (Math.random() * 0.5 + 0.25) + ')';
+
+    const date = document.createElement('div');
+    date.className = 'date';
+    date.innerHTML = table[data].date;
+    box.appendChild(date);
+
+    const photo = document.createElement('img');
+    photo.src = photoPath + table[data].photo;
+    photo.className = 'photo';
+    photo.addEventListener('pointerdown', () => {
+      var url = viewUrl + "?st=" + table[data].st + "&et=" + table[data].et + "&ct=" + table[data].ct + "&src=" + movPath + table[data].mov + parm;
+      window.open(url, '_blank')
+    });
+    box.appendChild(photo);
+
+    const objectCSS = new CSS3DObject(box);
+    objectCSS.position.x = (table[data].col * 450) - 1800;
+    objectCSS.position.y = - (table[data].row * 355) + 990;
+    // objectCSS.position.z = targets;
+    // objectCSS.position.x = Math.random() * 4000 - 2000;
+    // objectCSS.position.y = Math.random() * 4000 - 2000;
+    objectCSS.position.z = Math.random() * 4000 - 1000;
+    scene.add(objectCSS);
+
+    objects.push(objectCSS);
+
+    const object = new THREE.Object3D();
+    object.position.x = (table[data].col * 450) - 1800;
+    object.position.y = - (table[data].row * 355) + 990;
+
+    targets.table.push(object);
+  }
+
+  renderer = new CSS3DRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.getElementById('container').appendChild(renderer.domElement);
+
+  controls = new MapControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.enablePan  = true;
+  controls.dampingFactor = 0.05;
+  controls.autoRotate = false;
+  // controls.enableRotate = false;
+  controls.screenSpacePanning=  false;
+  controls.enableZoom = true;
+  controls.minDistance = 1500;
+  controls.maxDistance = 6000;
+  // How far you can orbit vertically, upper and lower limits.
+  // Range is 0 to Math.PI radians.
+  // controls.minPolarAngle =  Math.PI / 2;
+  // controls.maxPolarAngle = Math.PI / 2;
+
+  // How far you can orbit horizontally, upper and lower limits.
+  // If set, must be a sub-interval of the interval [ - Math.PI, Math.PI ].
+  controls.minAzimuthAngle = - Math.PI / 4; // radians
+  controls.maxAzimuthAngle = Math.PI / 4; // radians
+  controls.addEventListener('change', render);
+
+
+  transform(targets.table, 3000);
+  cameraMove(4000);
+
+  window.addEventListener('resize', onWindowResize);
+}
+
+document.getElementById("reload").addEventListener('click', () => {
+  cameraMove(2000);
+  controls.reset();
+})
+
+
+function transform(targets, duration) {
+  TWEEN.removeAll();
+  for (let i = 0; i < objects.length; i++) {
+    const object = objects[i];
+    const target = targets[i];
+    new TWEEN.Tween(object.position)
+      .to({ x: target.position.x, y: target.position.y, z: target.position.z }, Math.random() * duration + duration)
+      .easing(TWEEN.Easing.Exponential.InOut)
+      .start();
+
+    // new TWEEN.Tween( object.rotation )
+    // 	.to( { x: target.rotation.x, y: target.rotation.y, z: target.rotation.z }, Math.random() * duration + duration )
+    // 	.easing( TWEEN.Easing.Exponential.InOut )
+    // 	.start();
+
+  }
+  new TWEEN.Tween(this)
+    .to({}, duration * 2)
+    .onUpdate(render)
+    .start();
+}
+
+function cameraMove(ms) {
+  new TWEEN.Tween(camera.position)
+    .to({ x: 0, y: 0, z: 4500 }, ms)
+    .easing(TWEEN.Easing.Quintic.InOut)
+    .start();
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  render();
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  TWEEN.update();
+  controls.update();
+}
+
+function render() {
+  renderer.render(scene, camera);
+}
